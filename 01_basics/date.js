be4c736b297ba99f33199d0257acbe68da54918a
AparnaGoswami <152055714+AparnaGoswami@users.noreply.github.com>
2024-07-11 15:25:51 +0000

    commit

--- 01_basics/date.js
+++ 01_basics/date.js
@@ -1,2 +1,16 @@
 const myDate= new Date();
-console.log(myDate);
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.getHours());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toISOString());
+
+let newDate= new Date(2004,4,6)
+// console.log(newDate.toDateString());
+
+let ts=Date.now()
+console.log(ts);
+console.log(Math.floor(Date.now()/1000));
+console.log(myDate.toLocaleString('default',{
+    weekday:"short"
+}))
