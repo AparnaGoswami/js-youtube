1c4bb5de3248a90877e7e60043417c74bb5378e5
AparnaGoswami <152055714+AparnaGoswami@users.noreply.github.com>
2024-07-15 07:15:59 +0000

    commit

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -12,12 +12,39 @@ function subTwo(num1,num2) {
     return num1-num2
 }
 
-addTwo(3,4)
-console.log(subTwo(4,3))
+// addTwo(3,4)
+// console.log(subTwo(4,3))
 
 function login(username = "xyz"){
     return `${username} just logged in`
 }
 
-console.log(login("aparna"));
-console.log(login(""));
+// console.log(login("aparna"));
+// console.log(login(""));
+
+
+
+function cartPrice(...num1){
+    return num1;
+}
+//console.log(cartPrice(200,300,400));
+
+function cartPrice1(val1,val2,...num1) {
+    return num1;
+}
+//console.log(cartPrice1(100,200,300,400,500));
+
+const user={
+    name:"aparna",
+    price:599
+}
+function handleObj(obj) {
+    console.log(`Username is ${obj.name} and price is ${obj.price}`);
+}
+//handleObj(user)
+
+const arr=[200,400,600];
+function return2val(getArr) {
+    return getArr[1];
+}
+console.log(return2val(arr));
