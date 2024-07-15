1b094e30beda3d60a81547bdb79224feb5cabc80
AparnaGoswami <152055714+AparnaGoswami@users.noreply.github.com>
2024-07-15 17:49:12 +0000

    commit

---
+++ 03_basics/02_arrow.js
@@ -0,0 +1,42 @@
+const user={
+    name:"aparna",
+    email:"a@gmail.com",
+
+    welcomeMsg:function () {
+        console.log(`${this.name},welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMsg();
+// user.name="aanu"
+// user.welcomeMsg()
+
+//console.log(this);
+
+// function chai() {
+//     let username= "aparna"
+//     console.log(this);
+//     console.log(this.username);
+// }
+
+// const chai=function () {
+//     let username= "aparna"
+    
+//     console.log(this.username);
+// }
+
+
+const chai= () => {
+    let username= "aparna"
+    console.log(this);
+    console.log(this.username);
+}
+//chai()
+
+
+const addTwo = (num1,num2)=>{
+    return num1 + num2;
+}
+console.log(addTwo(3,4));
