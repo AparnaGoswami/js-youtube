068fed702a8c271045762dd81547abc6eb52c9db
AparnaGoswami <152055714+AparnaGoswami@users.noreply.github.com>
2024-08-01 06:24:18 +0000

    commit

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -50,12 +50,46 @@ promise4.then((user)=>{
 
 const promise5=new Promise((resolve, reject) => {
     setTimeout(()=>{
-        let error= false;
+        let error= true;
         if(!error){
-            resolve({username:"aparna",pass:"123"})
+            resolve({username:"javascript",pass:"123"})
         }else{
-            reject('ERROR: Something went wrong')
+            reject('ERROR: JS went wrong')
         }
     },1000)
 })
+async function consumePromise5(){
+    try{
+        const response = await promise5
+        console.log(response);
+    }catch(error){
+        console.log(error);
+    }
+}
 
+consumePromise5()
+
+
+async function getAllUsers() {
+        try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        
+            const data = await response.json()
+            console.log(data);
+        } catch (error) {
+            console.log("E: ", error);
+        }
+}
+getAllUsers()
+
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
+
+promise.all
